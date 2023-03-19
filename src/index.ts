f3853797a0f7f403c48ebcd5bb3ee3b41c589f15
Sam Chung <samc@seek.com.au>
2023-03-19 15:04:48 +1100

    Remove formatters

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,19 @@ import pino from 'pino';
 
 import base from './base';
 import { withRedaction } from './destination';
-import { FormatterOptions, createFormatters } from './formatters';
 import * as redact from './redact';
 import serializers from './serializers';
 
 export { pino };
 
-export type LoggerOptions = pino.LoggerOptions & FormatterOptions;
+export interface CustomLoggerOptions {
+  /**
+   * This allows finer control of redaction by providing access to the full text.
+   */
+  redactText?: (input: string, redactionPlaceholder: string) => string;
+}
+
+export type LoggerOptions = pino.LoggerOptions & CustomLoggerOptions;
 export type Logger = pino.Logger;
 
 /**
@@ -22,8 +28,6 @@ export default (
   // istanbul ignore next
   destination: pino.DestinationStream = pino.destination(1),
 ): Logger => {
-  const formatters = createFormatters(opts);
-
   opts.redact = redact.addDefaultRedactPathStrings(opts.redact);
   opts.serializers = {
     ...serializers,
@@ -32,10 +36,6 @@ export default (
   opts.base = {
     ...base,
     ...opts.base,
-  };
-  opts.formatters = {
-    ...formatters,
-    ...opts.formatters,
   };
 
   opts.timestamp ??= () => `,"timestamp":"${new Date().toISOString()}"`;
