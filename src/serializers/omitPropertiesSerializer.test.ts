d852b69fea261b254b6d53068f33a889b9703baa
Conrad Lang <clang@seek.com.au>
2023-10-04 21:17:26 +1100

    feat: Add omitPropertiesSerializer
    A higher order function for creating a serializer that omits properties
    from the object assigned to the top-level property.

---
+++ src/serializers/omitPropertiesSerializer.test.ts
@@ -0,0 +1,136 @@
+import type { Transform } from 'stream';
+
+import split from 'split2';
+
+import createLogger from '..';
+
+import { createOmitPropertiesSerializer } from './omitPropertiesSerializer';
+
+const omitPropertyNamesBase = ['remove-prop', 'remove.prop'];
+
+it.each`
+  scenario       | topLevelPropertyName
+  ${'undefined'} | ${undefined}
+  ${'null'}      | ${null}
+`(
+  'returns empty object when topLevelPropertyName is $scenario',
+  ({ topLevelPropertyName }) => {
+    const serializer = createOmitPropertiesSerializer(topLevelPropertyName, {
+      omitPropertyNames: [...omitPropertyNamesBase],
+    });
+
+    expect(serializer).toStrictEqual({});
+  },
+);
+
+it.each`
+  scenario                  | omitPropertyNames
+  ${'undefined'}            | ${undefined}
+  ${'null'}                 | ${null}
+  ${'empty'}                | ${[]}
+  ${'list without strings'} | ${[undefined, null, 1, true, false, {}, []]}
+`(
+  'returns empty object when omitPropertyNames is $scenario',
+  ({ omitPropertyNames }) => {
+    const serializer = createOmitPropertiesSerializer('main', {
+      omitPropertyNames,
+    });
+
+    expect(serializer).toStrictEqual({});
+  },
+);
+
+it('returns object with named property containing function', () => {
+  const serializer = createOmitPropertiesSerializer('main', {
+    omitPropertyNames: [...omitPropertyNamesBase],
+  });
+
+  expect(serializer).toStrictEqual({ main: expect.any(Function) });
+});
+
+const sink = () =>
+  split((data) => {
+    try {
+      return JSON.parse(data);
+    } catch (err) {
+      console.log(err); // eslint-disable-line
+      console.log(data); // eslint-disable-line
+    }
+  });
+
+function once(emitter: Transform, name: string) {
+  return new Promise((resolve, reject) => {
+    if (name !== 'error') {
+      emitter.once('error', reject);
+    }
+    emitter.once(name, (arg: unknown) => {
+      emitter.removeListener('error', reject);
+      resolve(arg);
+    });
+  });
+}
+
+it('omits properties from logged object', async () => {
+  const serializer = createOmitPropertiesSerializer('main', {
+    omitPropertyNames: [...omitPropertyNamesBase],
+  });
+
+  const input = {
+    main: {
+      keepProp: 'Some value',
+      ['remove-prop']: 'remove with dash',
+      ['remove.prop']: 'remove with dot',
+      remove: { prop: 'DO NOT REMOVE' },
+    },
+  };
+
+  const inputString = JSON.stringify(input);
+  const stream = sink();
+  const logger = createLogger(
+    { name: 'my-app', serializers: { ...serializer } },
+    stream,
+  );
+  logger.info(input);
+
+  const log: any = await once(stream, 'data');
+  expect(log).toMatchObject({
+    main: {
+      keepProp: 'Some value',
+      remove: { prop: 'DO NOT REMOVE' },
+    },
+  });
+  expect(inputString).toEqual(JSON.stringify(input));
+  expect(log).toHaveProperty('timestamp');
+});
+
+it.each`
+  scenario           | value               | expected
+  ${'undefined'}     | ${undefined}        | ${{}}
+  ${'null'}          | ${null}             | ${{ main: null }}
+  ${'not an object'} | ${123}              | ${{ main: 123 }}
+  ${'an array'}      | ${[{ value: 123 }]} | ${{ main: [{ value: 123 }] }}
+`(
+  'does nothing when top-level property is $scenario',
+  async ({ value, expected }) => {
+    const serializer = createOmitPropertiesSerializer('main', {
+      omitPropertyNames: ['keepProp'],
+    });
+
+    const input = {
+      main: value,
+    };
+
+    const inputString = JSON.stringify(input);
+    const stream = sink();
+    const logger = createLogger(
+      { name: 'my-app', serializers: { ...serializer } },
+      stream,
+    );
+    logger.info(input);
+
+    const log: any = await once(stream, 'data');
+    expect(log).toMatchObject(expected);
+    expect(inputString).toEqual(JSON.stringify(input));
+    expect(log).toHaveProperty('timestamp');
+  },
+);
---
+++ src/serializers/omitPropertiesSerializer.ts
@@ -0,0 +1,41 @@
+import { omitProperties } from './omitProperties';
+
+export interface OmitPropertiesSerializerOptions {
+  /**
+   * A list of properties that should not be logged.
+   */
+  omitPropertyNames: string[];
+}
+
+type OmitHeaderNamesFn = (
+  record: Record<string, unknown>,
+) => Record<string, unknown>;
+type OmitHeaderNamesSerializer = Record<string, OmitHeaderNamesFn>;
+
+/** Creates a serializer that operates on the logged object's top-level property named `topLevelPropertyName`
+ *  and omits the properties listed in `options.omitPropertyNames`.
+ *  @param topLevelPropertyName - The name of the root property on the logged object that to omit properties from.
+ *  @param options - Options for the serializer.
+ */
+export const createOmitPropertiesSerializer = (
+  topLevelPropertyName: string,
+  options: OmitPropertiesSerializerOptions,
+): OmitHeaderNamesSerializer => {
+  const propertyNames = [...new Set(options.omitPropertyNames ?? [])].filter(
+    (propertyName) => typeof propertyName === 'string',
+  );
+
+  if (
+    !topLevelPropertyName ||
+    typeof topLevelPropertyName !== 'string' ||
+    topLevelPropertyName.length === 0 ||
+    propertyNames.length === 0
+  ) {
+    return {};
+  }
+
+  return {
+    [topLevelPropertyName]: (record: Record<string, unknown>) =>
+      omitProperties(record, propertyNames),
+  };
+};
