2a74da2a9de12be912e8a5af83cd56bade9da513
samchungy <samchungy@gmail.com>
2025-06-12 14:35:26 +1000

    add silent

--- src/index.ts
+++ src/index.ts
@@ -56,6 +56,7 @@ export type Logger<CustomLevels extends string = never> = Omit<
   | 'info'
   | 'debug'
   | 'trace'
+  | 'silent'
   | 'child'
   | CustomLevels
 > & {
@@ -65,6 +66,7 @@ export type Logger<CustomLevels extends string = never> = Omit<
   info: LogFn;
   debug: LogFn;
   trace: LogFn;
+  silent: LogFn;
   child: <ChildCustomLevels extends string = never>(
     bindings: pino.Bindings,
     options?: pino.ChildLoggerOptions<ChildCustomLevels>,
