adf4a3fa8a02a8ac3248836181094b59b3d72ba5
Sam Chung <samc@seek.com.au>
2025-06-13 10:23:04 +1000

    Enforce stricter typing for logger methods (#182)
    * Add alternate types
    
    Co-authored-by: Zac Brydon <52645024+zbrydon@users.noreply.github.com>
    
    * Copilot ftw
    
    * lint ignore
    
    * add more changelog
    
    * tweak
    
    * Switch to unknown
    
    Co-authored-by: Zac Brydon <52645024+zbrydon@users.noreply.github.com>
    
    * Update .changeset/every-loops-help.md
    
    Co-authored-by: Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
    
    * Run `skuba format`
    
    * copilot go brrr
    
    Co-authored-by: Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
    
    * add silent
    
    * add missing level type
    
    * fix types
    
    * Use `err` instead of `error` as `error` has no serializer
    
    * address edge-case
    
    * Remove placeholder examples
    
    ---------
    
    Co-authored-by: Zac Brydon <52645024+zbrydon@users.noreply.github.com>
    Co-authored-by: Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
    Co-authored-by: skuba <34733141+seek-oss-ci@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import pino from 'pino';
+import pino, { type LoggerExtras } from 'pino';
 
 import base from './base';
 import { createDestination } from './destination/create';
@@ -15,8 +15,65 @@ export { pino };
 export type LoggerOptions<CustomLevels extends string = never> =
   pino.LoggerOptions<CustomLevels> & FormatterOptions & SerializerOptions;
 
-export type Logger<CustomLevels extends string = never> =
-  pino.Logger<CustomLevels>;
+type PlaceholderSpecifier = 'd' | 's' | 'j' | 'o' | 'O';
+type PlaceholderTypeMapping<T extends PlaceholderSpecifier> = T extends 'd'
+  ? number
+  : T extends 's'
+    ? string
+    : T extends 'j' | 'o' | 'O'
+      ? object
+      : never;
+
+type ParseLogFnArgs<
+  T,
+  Acc extends unknown[] = [],
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+> = T extends `${infer _}%${infer Placeholder}${infer Rest}`
+  ? Placeholder extends PlaceholderSpecifier
+    ? ParseLogFnArgs<Rest, [...Acc, PlaceholderTypeMapping<Placeholder>]>
+    : ParseLogFnArgs<Rest, Acc>
+  : Acc;
+
+// FIXME: Remove if pinojs/pino#2230 lands in a release.
+interface LogFn {
+  <T, TMsg extends string = string>(
+    obj: T,
+    msg?: T extends string ? never : TMsg,
+    ...args: ParseLogFnArgs<TMsg> | []
+  ): void;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  <_, TMsg extends string = string>(
+    msg: TMsg,
+    ...args: ParseLogFnArgs<TMsg> | []
+  ): void;
+}
+
+export type Logger<CustomLevels extends string = never> = Omit<
+  pino.Logger<CustomLevels>,
+  | 'fatal'
+  | 'error'
+  | 'warn'
+  | 'info'
+  | 'debug'
+  | 'trace'
+  | 'silent'
+  | 'child'
+  | CustomLevels
+> & {
+  level: pino.LevelWithSilentOrString;
+  fatal: LogFn;
+  error: LogFn;
+  warn: LogFn;
+  info: LogFn;
+  debug: LogFn;
+  trace: LogFn;
+  silent: LogFn;
+  child: <ChildCustomLevels extends string = never>(
+    bindings: pino.Bindings,
+    options?: pino.ChildLoggerOptions<ChildCustomLevels>,
+  ) => Logger<CustomLevels | ChildCustomLevels>;
+} & Record<CustomLevels, LogFn> &
+  LoggerExtras<CustomLevels>;
 
 /**
  * Creates a logger that can enforce a strict logged object shape.
