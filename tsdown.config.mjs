88b3aa4cddbcd44ed47d55d8b476c21fda5e1143
samchungy <samchungy@gmail.com>
2026-02-13 17:06:51 +1100

    bump things

--- tsdown.config.mjs
+++ tsdown.config.mjs
@@ -5,8 +5,5 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   exports: true,
-  dts: {
-    // Bundles type declarations for the specified packages
-    resolve: ['sury'],
-  },
+  inlineOnly: ['sury'],
 });
