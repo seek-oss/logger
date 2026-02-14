57d37bdaac5a412da64dd093f7102db2c562a822
Sam Chung <samchungy@gmail.com>
2026-02-14 12:29:26 +1100

    Reduce Sury Size (#289)
    * Sury not sury
    
    * bump tsdown
    
    * bump things
    
    * update

--- tsdown.config.mjs
+++ tsdown.config.mjs
@@ -1,11 +1,9 @@
+// @ts-check
 import { defineConfig } from 'tsdown/config';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   exports: true,
-  dts: {
-    // Bundles type declarations for the specified packages
-    resolve: ['sury'],
-  },
+  inlineOnly: ['sury'],
 });
